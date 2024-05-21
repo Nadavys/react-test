93e58adcb3cb0167c7f4ee1d2ba752436ae71ec7
Nadav Somekh <nadavys@gmail.com>
2024-05-20 17:21:36 -0700

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Demo test project</h1>
+      <h1>Demo test project updated!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
